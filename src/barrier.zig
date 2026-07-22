1f24feb2f10033d3039b9182ec4d545f711c37cf
thehxdev <hossein.khosravi.ce@gmail.com>
2026-07-22 20:08:59 +0330

    barrier.zig: refactor

--- src/barrier.zig
+++ src/barrier.zig
@@ -3,28 +3,28 @@ const Io = std.Io;
 const Condition = Io.Condition;
 const Mutex = Io.Mutex;
 
-pub const Barrier = struct {
-    mutex: Mutex = .init,
-    cond: Condition = .init,
-    generation: usize = 0,
-    count: usize = 0,
-    total: usize,
+pub const Barrier = @This();
 
-    pub fn init(total: usize) Barrier {
-        return .{ .total = total };
-    }
+mutex: Mutex = .init,
+cond: Condition = .init,
+generation: usize = 0,
+count: usize = 0,
+total: usize,
 
-    pub fn wait(self: *Barrier, io: Io) !void {
-        try self.mutex.lock(io);
-        defer self.mutex.unlock(io);
-        const gen = self.generation;
-        self.count += 1;
-        if (self.count == self.total) {
-            self.count = 0;
-            self.generation +%= 1;
-            self.cond.broadcast(io);
-        } else {
-            while (gen == self.generation) try self.cond.wait(io, &self.mutex);
-        }
+pub fn init(total: usize) Barrier {
+    return .{ .total = total };
+}
+
+pub fn wait(self: *Barrier, io: Io) !void {
+    try self.mutex.lock(io);
+    defer self.mutex.unlock(io);
+    const gen = self.generation;
+    self.count += 1;
+    if (self.count == self.total) {
+        self.count = 0;
+        self.generation +%= 1;
+        self.cond.broadcast(io);
+    } else {
+        while (gen == self.generation) try self.cond.wait(io, &self.mutex);
     }
-};
+}
